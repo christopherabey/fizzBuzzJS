2aacfcd7bbce8aaf77863890774a30e547c85e45
Chris Abey <66890277+christopherabey@users.noreply.github.com>
2021-07-31 16:21:32 -0600

    Update fizzBuzz.js
    added a while loop to give the user the option to run the sequence for as long as they would like to see

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,29 +1,36 @@
 var output = [];
 
-number = 1;
+var number = 1;
+var count = 1;
+var upperLimit = prompt("What should be the upper bounds of the FizzBuzz sequence?");
 
 function fizzBuzz() {  
+    
+    while (count <=upperLimit) {
 
-    output.push(number);
+        output.push(number);
 
-    if (number % 3 === 0) {
-        output.pop();
-        output.push("Fizz");
+        if (number % 3 === 0) {
+            output.pop();
+            output.push("Fizz");
+        }
+
+        if (number % 5 === 0) {
+            output.pop();
+            output.push("Buzz");
+        }
+
+        if (number % 3 === 0 && number % 5 === 0) {
+            output.pop();
+            output.push("FizzBuzz");
+        }
+
+        number++;
+
+        count++;
     }
-
-    if (number % 5 === 0) {
-        output.pop();
-        output.push("Buzz");
-    }
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        output.pop();
-        output.push("FizzBuzz");
-    }
-
-    number++;
-
-    console.log(output);
+    
+    alert(output);
 }
 
 fizzBuzz();
